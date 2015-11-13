2df95cac7f70f517c310cc3ada655744aa80965e
zallen3 <zach.allen.333@gmail.com>
2015-11-12 21:57:43 -0600

    created views for login/registration setup js using parse for authentication for login/registration added parse information

--- app/components/login/loginController.js
+++ app/components/login/loginController.js
@@ -1,3 +1,27 @@
 /**
  * Created by user on 11/11/15.
  */
+Parse.initialize("JlLvffl08QrBwOGUBp3RiSE0ppdWIR9dceOnx0r8",
+    "5VjICKXhdqa1jIlqWEhcdILiTfIu8PFRa7tMvLsU");
+
+angular.module('app', [])
+    .run(['$rootScope', function($scope) {
+        $scope.currentUser = Parse.User.current();
+
+        $scope.logIn = function(form) {
+            Parse.User.logIn(form.username, form.password, {
+                success: function(user) {
+                    $scope.currentUser = user;
+                    $scope.$apply();
+                },
+                error: function(user, error) {
+                    alert("Unable to log in: " + error.code + " " + error.message);
+                }
+            });
+        };
+
+        $scope.logOut = function(form) {
+            Parse.User.logOut();
+            $scope.currentUser = null;
+        };
+    }]);
--- app/components/login/registrationController.js
+++ app/components/login/registrationController.js
@@ -1,3 +1,32 @@
 /**
  * Created by user on 11/12/15.
  */
+Parse.initialize("JlLvffl08QrBwOGUBp3RiSE0ppdWIR9dceOnx0r8",
+    "5VjICKXhdqa1jIlqWEhcdILiTfIu8PFRa7tMvLsU");
+
+angular.module('app', [])
+    .run(['$rootScope', function($scope) {
+        $scope.currentUser = Parse.User.current();
+
+        $scope.register = function(form) {
+            var user = new Parse.User();
+            user.set("email", form.email);
+            user.set("username", form.username);
+            user.set("password", form.password);
+
+            user.register(null, {
+                success: function(user) {
+                    $scope.currentUser = user;
+                    $scope.$apply(); // Notify AngularJS to sync currentUser
+                },
+                error: function(user, error) {
+                    alert("Unable to register:  " + error.code + " " + error.message);
+                }
+            });
+        };
+
+        $scope.logOut = function(form) {
+            Parse.User.logOut();
+            $scope.currentUser = null;
+        };
+    }]);
